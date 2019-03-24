cd6c4e40a36d83817f9c591d0fb20d6d2d78b2b7
VoropaiVadim <vadim.w.i@rambler.ru>
2019-03-24 20:50:40 +0300

    docs:task start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-
-
+  const newMatrix = matrix;
+  for(let row = 0; row < 9; row++) {
+      for(let col = 0; col < 9; col++) {
+        if (matrix[row][col] === 0) {
+          const suggs = count(row, col, newMatrix);
+          for(let sugg of suggs) {
+            newMatrix[row][col] = sugg;
+            solveSudoku(newMatrix);
+          } 
+        }
+     }
+  }
+  return newMatrix;
+  }
   
-}
+  function count(row, col, matrix) {
+      const suggs = [];
+      row = help(row) * 3;
+      col = help(col) * 3;
+      for(let i = 0; i < 9; i++) {
+        suggs.push([matrix[row][i], matrix[i][col], matrix [row + i % 3][col + help(i)]]);
+  
+      }
+  return suggs;
+  }
+  
+  function help(num) {
+    return Math.floor(num/3)  
+  }
