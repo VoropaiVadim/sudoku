69f8abafaeaa7122068b58b34f6749325784e0cb
VoropaiVadim <vadim.w.i@rambler.ru>
2019-03-24 21:05:35 +0300

    feat:add final file

--- src/index.js
+++ src/index.js
@@ -1,90 +1,36 @@
+
 module.exports = function solveSudoku(matrix) {
-
-  var possibleSolutions = [];
-  for (let i = 0; i < 9; i++) {
-    possibleSolutions[i] = [];
-    for (let j = 0; j < 9; j++) {
-      possibleSolutions[i][j] = [];
-    }
-  }
-  var combination = [];
-  for (let i = 0; i < 9; i++) {
-    combination[i] = [];
-    for (let j = 0; j < 9; j++) {
-      combination[i][j] = 0;
-    }
-  }
-  var solution = [];
-  for (let i = 0; i < 9; i++) {
-    solution[i] = [];
-  }
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      solution[i][j] = matrix[i][j];
-    }
-  }
-
-  //looking for possible solutions
-  for (let i = 0; i < 9; i++)
-    for (let j = 0; j < 9; j++)
-      if (!matrix[i][j]) {
-          for (let k = 0; k <= 9; k++)
-            if (isUnique(matrix, i, j, k)) { possibleSolutions[i][j].push(k); }
-      } 
-
-  //trying different combinations
-  var counter = 0;
-  var flag = true;
-  while (flag) {
-    flag = false;
-    for (var i = 0; i < 9; i++) {
-      for (var j = 0; j < 9; j++) {
-        if (!matrix[i][j])
-          if (!isUnique(solution, i, j, solution[i][j]) || !solution[i][j]) {
-            for (combination[i][j] = 0; combination[i][j] < possibleSolutions[i][j].length; combination[i][j]++) {
-              if (isUnique(solution, i, j, possibleSolutions[i][j][combination[i][j]])) {
-                solution[i][j] = possibleSolutions[i][j][combination[i][j]];
-                break;
+  const matrixDeepCopy = JSON.parse(JSON.stringify(matrix));
+  for(let position_row=0;position_row<9;position_row++)
+  for(let position_col=0;position_col<9;position_col++)
+  {
+      if(matrixDeepCopy[position_row][position_col]===0){
+          let solutionScope = findSolution(position_row,position_col,matrixDeepCopy);
+          if(solutionScope.length==0){
+            return false;
+          }
+          for(let solution of solutionScope){
+              matrixDeepCopy[position_row][position_col] = solution;
+              var result = solveSudoku(matrixDeepCopy);
+              if(result!=false){
+                return solveSudoku(result);
               }
-            }
-            if (!solution[i][j] || !isUnique(solution, i, j, solution[i][j])) {
-              solution[i][j] = possibleSolutions[i][j][Math.floor((possibleSolutions[i][j].length)*Math.random())];
-              flag = true;
-            }
+          }
+          return false;
+      }
+  }
+  return  matrixDeepCopy;
+}
+
+function findSolution(position_row,position_col,matrix){
+  const scope = [];
+  label: for(let i=1;i<10;i++){
+      for(let j=0;j<9;j++){
+          if(matrix[position_row][j]==i || matrix[j][position_col]==i || matrix[Math.floor(position_row/3)*3+j%3][Math.floor(position_col/3)*3+Math.floor(j/3)]==i){
+              continue label;
           }
       }
-    }
+      scope.push(i);
   }
-
-  
-  function isUnique(matrix, i_pos, j_pos, value) {
-    for (let i = 0; i < 9; i++)
-      if (matrix[i][j_pos] == value && i != i_pos) return false;
-    for (let j = 0; j < 9; j++)
-      if (matrix[i_pos][j] == value && j != j_pos) return false;
-    var i_block = 3*Math.floor(i_pos/3);
-    var j_block = 3*Math.floor(j_pos/3);
-    for (let i = i_block; i < i_block + 3; i++)
-      for (let j = j_block; j < j_block + 3; j++)
-        if (matrix[i][j] == value && i != i_pos && j != j_pos) return false;
-    return true;
-  }
-
-  function countDismatches(matrix, i_pos, j_pos, value) {
-    var counter = 0;
-    for (let i = 0; i < 9; i++)
-      if (matrix[i][j_pos] == value && i != i_pos) ++counter;
-    for (let j = 0; j < 9; j++)
-      if (matrix[i_pos][j] == value && j != j_pos) ++counter;
-    var i_block = 3*Math.floor(i_pos/3);
-    var j_block = 3*Math.floor(j_pos/3);
-    for (let i = i_block; i < i_block + 3; i++)
-      for (let j = j_block; j < j_block + 3; j++)
-        if (matrix[i][j] == value && i != i_pos && j != j_pos) ++counter;
-    return counter;
-  }
-
-  // console.log(solution);
-
-  return solution;
+return scope;
 }
